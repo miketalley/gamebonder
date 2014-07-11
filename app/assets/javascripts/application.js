75fb3dec0f1abe28dd7ebf3d4ef6602382e6ae5b
Mike Talley <miketalley@hotmail.com>
2014-07-11 11:56:29 -0400

    Added back turbolinks jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-turbolinks
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require angular
 //= require angular-animate
