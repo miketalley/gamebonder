dad2e2af7499c118e1f298db25b28a97124880f0
Mike Talley <miketalley@hotmail.com>
2014-07-15 15:29:55 -0400

    Reverted index

--- app/models/game.rb
+++ app/models/game.rb
@@ -15,21 +15,17 @@ class Game < ActiveRecord::Base
 
       if !children[:bonds].include?(bond)
         children[:bonds].push(bond)
-        puts "Adding Bond: #{bond.id}"
 
         if !children[:games].include?(bond.source)
           children[:games].push(bond.source)
-          puts "Adding Game: #{bond.source.name}"
         end
 
         if !children[:games].include?(bond.target)
           children[:games].push(bond.target)
-          puts "Adding Game: #{bond.target.name}"
         end
 
         if bond.source.id != self.id && bond.source.bonds.length > 0 && !(@prior.include?(bond.source))
             @prior << bond.source
-            puts "Recursion on #{bond.source}"
             tmp = get_children(bond.source)
             children[:games] += tmp[:games]
             children[:bonds] += tmp[:bonds]
@@ -37,7 +33,6 @@ class Game < ActiveRecord::Base
 
         if bond.target.id != game.id && bond.target.bonds.length > 0 && !(@prior.include?(bond.target))
             @prior << bond.target
-            puts "Recursion on #{bond.target}"
             tmp = get_children(bond.target)
             children[:games] += tmp[:games]
             children[:bonds] += tmp[:bonds]
@@ -50,10 +45,6 @@ class Game < ActiveRecord::Base
     children[:games].uniq!
     children[:bonds].uniq!
     return children
-  end
-
-  def self.children_bonds
-    puts 'hola'
   end
 
 end
