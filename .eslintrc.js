bdbed0f843468b2d99c4ee981f169117b63be327
Mike Talley <miketalley@hotmail.com>
2019-06-22 13:18:24 -0400

    installed vuetify and fixed linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    '@vue/airbnb'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-extra-semi': 'error',
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', {
+      'arrays': 'never',
+      'objects': 'never',
+      'imports': 'always',
+      'exports': 'always',
+      'functions': 'never'
+  }]
   },
   parserOptions: {
     parser: 'babel-eslint',
-  },
+    ecmaVersion: 6
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,5 +9,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app');
